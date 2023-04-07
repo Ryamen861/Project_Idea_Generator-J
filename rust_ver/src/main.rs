7010c4061720caafb5549c3923552e80de6a4a52
Ryan Moon <ryanmoon2050@gmail.com>
2023-04-07 14:14:51 -0400

    readme

--- rust_ver/src/main.rs
+++ rust_ver/src/main.rs
@@ -4,5 +4,6 @@ use rand::Rng;
 fn main() {
     println!("Welcome to the Project Idea Generator!");
 
-
+    let index = rand::thread_rng().gen_range(1..10);
+    println!("the index is {}", index);
 }
